042e764469fd13335e0c34dd328da585e1f09aee
Oliver Squibb <oliver.squibb@therapyappointment.com>
2021-05-09 18:30:34 -0400

    uninstalled body-parser

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const session = require("express-session")
-const bodyParser = require("body-parser")
 const passport = require('passport')
 const LocalStrategy = require("passport-local").Strategy
 
 const app = express()
 
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(passport.initialize())
 app.use(passport.session())
 
