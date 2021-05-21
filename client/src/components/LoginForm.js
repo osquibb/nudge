de64903c2e9ead7ddb3eac4cb4dd9119baad801b
Oliver Squibb <oliver.squibb@therapyappointment.com>
2021-05-20 21:47:09 -0400

    client login and health api calls

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 export default function LoginForm(props) {
 
@@ -6,35 +7,46 @@ export default function LoginForm(props) {
     username: '',
     password: ''
   })
+  const [health, setHealth] = useState('N/A')
 
   const onUsernameChange = username => setUser({
     ...user,
     username
   })
   
-
   const onPasswordChange = password => setUser({
     ...user,
     password
   })
 
+  const onSignIn = () => axios.post('/login', user)
+  const onHealthCheck = async () => {
+    const { data: { health } } = await axios.get('/health')
+    setHealth(health || 'N/A')
+  }
+
   return(
     <div>
       <h3>Login</h3>
-      <form>
-        <input
-          type="text"
-          placeholder="Username"
-          value={user.username}
-          onChange={e => onUsernameChange(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={user.password}
-          onChange={e => onPasswordChange(e.target.value)}
-        />
-      </form>
+      <input
+        type="text"
+        placeholder="Username"
+        value={user.username}
+        onChange={e => onUsernameChange(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={user.password}
+        onChange={e => onPasswordChange(e.target.value)}
+      />
+      <button onClick={onSignIn}>
+        Sign In
+      </button>
+      <button onClick={onHealthCheck}>
+        Health Check
+      </button>
+      <p>{health}</p>
     </div>
   )
 }
