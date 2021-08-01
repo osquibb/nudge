754eba24df0c9bb003feb45c28644e7910cfff6b
Oliver Squibb <oliver.squibb@therapyappointment.com>
2021-08-01 17:07:22 -0400

    gameservice formatting

--- server/services/gameService.js
+++ server/services/gameService.js
@@ -4,15 +4,53 @@ module.exports = {
   listGames: async () => await db('games').select(),
 
   listGamesAndJoinedStatusByUserId: async user_id => {
-  const resultSet = await db.raw('SELECT id, title, latitude, longitude, expiration, user_id IS NOT NULL as is_joined FROM games LEFT OUTER JOIN user_games ON games.id = user_games.game_id WHERE user_id IS NULL OR user_id = ?', [user_id])
-  return resultSet.rows
+    const resultSet = await db.raw(
+      `
+      SELECT
+        id,
+        title,
+        latitude,
+        longitude,
+        expiration,
+        user_id IS NOT NULL as is_joined
+      FROM
+        games
+      LEFT OUTER JOIN
+        user_games
+      ON
+        games.id = user_games.game_id
+      WHERE
+        user_id IS NULL OR user_id = ?
+      `,
+      [user_id]
+    )
+    return resultSet.rows
   },
 
   listGamesByUserId : async user_id =>
     await db('games').innerJoin('user_games', 'games.id', 'user_games.game_id').where({ user_id }),
 
   findGameAndJoinedStatusByGameIdAndUserId: async (game_id, user_id) => {
-    const resultSet = await db.raw('SELECT id, title, latitude, longitude, expiration, user_id IS NOT NULL as is_joined FROM games LEFT OUTER JOIN user_games ON games.id = user_games.game_id WHERE id = ? AND (user_id IS NULL OR user_id = ?)', [game_id, user_id])
+    const resultSet = await db.raw(
+      `
+      SELECT
+        id,
+        title,
+        latitude,
+        longitude,
+        expiration,
+        user_id IS NOT NULL as is_joined
+      FROM
+        games
+      LEFT OUTER JOIN
+        user_games
+      ON
+        games.id = user_games.game_id
+      WHERE
+        id = ? AND (user_id IS NULL OR user_id = ?)
+      `,
+      [game_id, user_id]
+    )
     return resultSet.rows[0]
   },
 
@@ -47,7 +85,26 @@ module.exports = {
       }
       await trx('games').where({ id: game_id }).update({ latitude, longitude })
       await trx('user_games').where({ user_id, game_id }).update({ last_nudge_at: new Date() }).returning(['game_id', 'user_id', 'last_nudge_at'])
-      const resultSet = await trx.raw('SELECT id, title, latitude, longitude, expiration, user_id IS NOT NULL as is_joined FROM games LEFT OUTER JOIN user_games ON games.id = user_games.game_id WHERE id = ? AND (user_id IS NULL OR user_id = ?)', [game_id, user_id])
+      const resultSet = await trx.raw(
+        `
+        SELECT
+          id,
+          title,
+          latitude,
+          longitude,
+          expiration,
+          user_id IS NOT NULL as is_joined
+        FROM
+          games
+        LEFT OUTER JOIN
+          user_games
+        ON
+          games.id = user_games.game_id
+        WHERE
+          id = ? AND (user_id IS NULL OR user_id = ?)
+        `,
+        [game_id, user_id]
+      )
       return resultSet.rows[0]
     })
 }
