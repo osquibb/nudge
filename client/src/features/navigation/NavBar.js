97055adfee9368dcf3953602ecdfe594a4fa7dc8
Oliver Squibb <oliver.squibb@therapyappointment.com>
2021-08-14 15:43:43 -0400

    n/a

--- client/src/features/navigation/NavBar.js
+++ client/src/features/navigation/NavBar.js
@@ -1,4 +1,4 @@
-import { React, useState } from 'react'
+import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectUser, logout } from '../user/userSlice'
